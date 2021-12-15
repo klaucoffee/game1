5cc12bd046f913d1f6ed41a0900dedb6f15370b6
klau <krystlelau90@gmail.com>
2021-12-15 21:43:36 +0800

    added map with floor

--- game1.js
+++ game1.js
@@ -12,7 +12,7 @@ loadSprite('tie1', '2wogbvE.png')
 loadSprite('tie3', 'JI7AM9I.png')
 loadSprite('novel2', 'CC4Qfce.png')
 loadSprite('novel1', 'Zzi0tLe.png')
-loadSprite('date1', 'pgE4uC.png')
+loadSprite('date1', 'rpgE4uC.png')
 loadSprite('date2', '5I345Tw.png')
 loadSprite('fall2', '0IQw0ro.png')
 loadSprite('fall1', 'WCqOl0x.png')
@@ -27,7 +27,28 @@ loadSprite('me', 'pbu3a1q.png')
 scene("game", () => {
     layers(['bg', 'obj', 'ui'], 'obj')
 
+    const map = [
 
+        '                                                               ',
+        '                                                               ',
+        '                     @@@@@!@@@@@@!@@@@!@                       ',
+        '                                                               ',
+        '                                                               ',
+        '           @@@@@#@@@@@@##@@@@@                                 ',
+        '                                                               ',
+        '                                                               ',
+        '                                     @@?@@@?@@@@               ',
+        '                                                               ',
+        '==================================================    =========',
+    ]
+
+    const levelCfg = {
+        width: 40,
+        height: 40,
+        '=': [sprite('floor', solid())]
+    }
+
+    const gameLevel = addLevel(map, levelCfg)
 
 })
 
