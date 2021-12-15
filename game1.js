afcbacd39ca4b26152c00157591701fe3694b6f4
klau <krystlelau90@gmail.com>
2021-12-15 23:48:54 +0800

    randomized pictures

--- game1.js
+++ game1.js
@@ -1,7 +1,7 @@
 kaboom({
     global: true,
     fullscreen: true,
-    scale: 2,
+    scale: 1,
     debug: true,
     clearColor: [0, 0, 0, 1],
 })
@@ -12,16 +12,16 @@ loadSprite('tie1', '2wogbvE.png')
 loadSprite('tie3', 'JI7AM9I.png')
 loadSprite('novel2', 'CC4Qfce.png')
 loadSprite('novel1', 'Zzi0tLe.png')
-loadSprite('date1', 'rpgE4uC.png')
+loadSprite('date1', 'DHWMOOo.png')
 loadSprite('date2', '5I345Tw.png')
 loadSprite('fall2', '0IQw0ro.png')
 loadSprite('fall1', 'WCqOl0x.png')
-loadSprite('brick', 'zqJGPqm.png')
+loadSprite('brick', 'aE5Dxen.png')
 loadSprite('floor', 'QXYmhyn.png')
 loadSprite('wall', 'UujO9Sp.png')
-loadSprite('gold', '3alcNAk.png')
-loadSprite('evil', 'CKT1ipY.png')
-loadSprite('me', 'pbu3a1q.png')
+loadSprite('gold', 'P0dL3BH.png')
+loadSprite('evil', 'qVMNAmN.png')
+loadSprite('me', 'c19OsFc.png.png')
 
 
 scene("game", () => {
@@ -29,23 +29,31 @@ scene("game", () => {
 
     const map = [
 
-        '                                                               ',
-        '                                                               ',
-        '                     @@@@@!@@@@@@!@@@@!@                       ',
-        '                                                               ',
-        '                                                               ',
-        '           @@@@@#@@@@@@##@@@@@                                 ',
-        '                                                               ',
-        '                                                               ',
-        '                                     @@?@@@?@@@@               ',
-        '                                                               ',
-        '==================================================    =========',
+        '$                           $',
+        '$    @@@@+@@@#@@@@@         $',
+        '$                           $',
+        '$                           $',
+        '$ @@@@@!@@@@@              $',
+        '$                           $',
+        '$                           $',
+        '$        @@?@@@&@@@@        $',
+        '$                           $',
+        '$         ^              ^  $',
+        '==================    ========',
     ]
 
     const levelCfg = {
-        width: 40,
-        height: 40,
-        '=': [sprite('floor', solid())]
+        width: 100,
+        height: 100,
+        '=': [sprite('floor', solid())],
+        '@': [sprite('brick', solid())],
+        '$': [sprite('wall', solid())],
+        '^': [sprite('evil', solid())],
+        '!': [sprite(choose(['fall1', 'date1', 'tie1', 'novel1']))],
+        '?': [sprite(choose(['fall1', 'date1', 'tie1', 'novel1']))],
+        '#': [sprite(choose(['fall2', 'date2', 'tie2', 'novel2']))],
+        '+': [sprite(choose(['fall2', 'date2', 'tie2', 'novel2']))],
+        '&': [sprite('tie3', solid())],
     }
 
     const gameLevel = addLevel(map, levelCfg)
