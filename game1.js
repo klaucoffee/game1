c8bb4dcf77de2d46f99c397b5861aec7fd62fa7e
klau <krystlelau90@gmail.com>
2021-12-15 21:24:46 +0800

    added images self drawn

--- game1.js
+++ game1.js
@@ -3,11 +3,32 @@ kaboom({
     fullscreen: true,
     scale: 2,
     debug: true,
-    clearColor: [0, 0, 1, 1],
+    clearColor: [0, 0, 0, 1],
 })
 
-scene("game", ({ level, score }) => {
+loadRoot('https://i.imgur.com/')
+loadSprite('tie2', 'ddxq5LU.png')
+loadSprite('tie1', '2wogbvE.png')
+loadSprite('tie3', 'JI7AM9I.png')
+loadSprite('novel2', 'CC4Qfce.png')
+loadSprite('novel1', 'Zzi0tLe.png')
+loadSprite('date1', 'pgE4uC.png')
+loadSprite('date2', '5I345Tw.png')
+loadSprite('fall2', '0IQw0ro.png')
+loadSprite('fall1', 'WCqOl0x.png')
+loadSprite('brick', 'zqJGPqm.png')
+loadSprite('floor', 'QXYmhyn.png')
+loadSprite('wall', 'UujO9Sp.png')
+loadSprite('gold', '3alcNAk.png')
+loadSprite('evil', 'CKT1ipY.png')
+loadSprite('me', 'pbu3a1q.png')
+
+
+scene("game", () => {
     layers(['bg', 'obj', 'ui'], 'obj')
+
+
+
 })
 
 
